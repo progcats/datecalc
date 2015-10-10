9557e87e0a6c9fb73155d89f41ce3260f0836118
afthar <afthar@afthar.com>
2015-10-10 21:39:30 +1300

    Added test class for Lisa

--- wwwcalc.py
+++ wwwcalc.py
@@ -12,6 +12,12 @@ class index:
     def GET(self):
         return calc.mo12('ilya')
 
+
+class test:
+    def GET(self):
+        return calc.mo12('Lisa')
+
+
 if __name__ == "__main__":
     app = web.application(urls, globals())
-    app.run()
+    app.run() 
