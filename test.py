6ff26220e549435a8299d9922c09961aac88da30
afthar <afthar@afthar.com>
2015-10-11 00:01:05 +1300

    add unit test Lisa

--- test.py
+++ test.py
@@ -6,6 +6,8 @@ class AddTest(unittest.TestCase):
 	def test_positive_add(self):
 		self.assertEqual(calc.mo12('ww'), 'ww, ddd, 2009-12-12')
 
+	def test_pos_add(self):
+		self.assertEqual(calc.mo12('Lisa'), 'Lisa, Zyuzina, 2000-04-24')
 
 if __name__=='__main__':
 	calc = Datecalc()
